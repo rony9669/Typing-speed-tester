1ab52130fd5401df5638d1475765bcecb829af9b
rony9669 <ronychowdhury.cse@gmail.com>
2022-09-15 00:10:33 +0600

    alart modal on page middle problem solved

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ const gameOver = () => {
     // the current time is the finish time
     // so total time taken is current time - start time
     const finishTime = new Date().getTime();
-    const timeTaken = (finishTime - startTime) / 1000;
+    const timeTaken = parseInt((finishTime - startTime) / 1000);
 
     // show result modal
     resultModal.innerHTML = "";
@@ -81,7 +81,7 @@ const gameOver = () => {
     // clear user text
     display.innerHTML = "";
     // make it inactive
-    display.classList.add("inactive");
+    display.classList.add("inactive"); // error in time taken
     // show result
     resultModal.innerHTML += `
     <h1>Finished!</h1>
@@ -140,7 +140,7 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
     const currentTime = new Date().getTime();
-    const timeSpent = (currentTime - startTime) / 1000;
+    const timeSpent = parseInt((currentTime - startTime) / 1000);
 
     document.getElementById("show-time").innerHTML = `${
     startTime ? timeSpent : 0
